8b208ee4c3a7b18ce28648fb61ac6c83b86a2f96
ZNair <zayne@roqcx.com>
2025-04-15 10:56:01 +0400

    chore: simplify ESLint configuration and remove unused dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import nextPlugin from '@next/eslint-plugin-next'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
 export default [
@@ -14,39 +15,46 @@ export default [
       'coverage/**',
       'public/**',
       '*.config.{js,ts}',
-      '**/*.d.ts'
+      '**/*.d.ts',
     ],
     plugins: {
       '@next/next': nextPlugin,
-      '@typescript-eslint': typescriptPlugin,
-      'react-hooks': reactHooksPlugin
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooksPlugin,
     },
     languageOptions: {
-      parser: typescriptPlugin.parser,
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: {
-          jsx: true
-        }
-      }
+          jsx: true,
+        },
+      },
+      globals: {
+        React: 'readonly',
+      },
+    },
+    settings: {
+      next: {
+        rootDir: '.',
+      },
     },
     rules: {
-      // Next.js rules
+      // Next.js core rules
       '@next/next/no-html-link-for-pages': 'error',
-      '@next/next/no-img-element': 'warn',
-      '@next/next/no-typos': 'error',
-      '@next/next/no-unwanted-polyfillio': 'error',
-      
+      '@next/next/no-img-element': 'error',
+
       // TypeScript rules
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      
-      // React rules
+
+      // React Hooks rules
       'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn'
-    }
-  }
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // General rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
 ]
